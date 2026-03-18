26a00f1ce6a2805a8410bfed55ee1ff8d03367f1
Sebastiano Bisacchi <sbisacchi@materys.com>
2026-03-18 16:22:27 +0100

    Fixed ERROR_IONIC_INTERRUPTED_PARTIAL_TRAJECTORY handling This handler was trying to resuse an output (structure_data) that may not be always present

--- src/aiida_quantumespresso/workflows/pw/base.py
+++ src/aiida_quantumespresso/workflows/pw/base.py
@@ -502,7 +502,9 @@ class PwBaseWorkChain(ProtocolMixin, BaseRestartWorkChain):
         transient, so we can restart from the last output structure. Note that since the job got interrupted the charge
         density and wave functions are likely corrupt so those cannot be used in the restart.
         """
-        self.ctx.inputs.structure = calculation.outputs.output_structure
+        if 'output_structure' in calculation.outputs:
+            self.ctx.inputs.structure = calculation.outputs.output_structure
+
         self.set_restart_type(RestartType.FROM_SCRATCH)
         self.report_error_handled(calculation, 'restarting from scratch from the last output structure')
         return ProcessHandlerReport(True)
